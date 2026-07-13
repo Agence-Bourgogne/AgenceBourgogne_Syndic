41e00dea1d97a1299ec87ec5ebad4a25a8c38fc8
Enzo Sandre <enzo@enzosandre.fr>
2026-07-13 11:56:56 +0200

    Imported Legacy ServiceDocuments

---
+++ ServiceDocuments/App_Code/IService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.IO;
+using SyndicDocumentsData.Entities;
+// REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IService" à la fois dans le code et le fichier de configuration.
+[ServiceContract]
+public interface IService
+{
+
+    [OperationContract]
+    string CreateUser(string Code, String Password );
+
+    [OperationContract]
+    void UploadPartFile(string Guid, byte[] filePart);
+
+    [OperationContract]
+    String CloseFile(string text, string Guid, string immeuble_id, string copro_id);
+
+    [OperationContract]
+    UserEntitie GetUserFromCode(String code);
+
+    [OperationContract]
+    UserEntitie GetUserFromGuid(String Guid);
+
+    [OperationContract]
+    List<UserEntitie> GetAllUsers();
+
+    [OperationContract]
+    List<ChildrenEntitie> GetUserCoproprietaires(string guid);
+
+    [OperationContract]
+    string AddCopro(string user_id, string immeuble_id, String copro_id, string reference_imm, string adresse_imm, string reference_copro, string nom);
+
+    [OperationContract]
+    string DeleteUser(string guid);
+
+    [OperationContract]
+    string DeleteCopro(string guid);
+
+    [OperationContract]
+    List<DocumentEntitie> GetListDocuments(string immeuble_id = "", string copro_id = "");
+
+    [OperationContract]
+    string DeleteDocument(string guid);
+
+    [OperationContract]
+    bool Connected();
+
+    [OperationContract]
+    string UpdateUser(string guid, string Code, String Password);
+
+}
+
---
+++ ServiceDocuments/App_Code/Service.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.IO;
+using SyndicDocumentsData.Entities;
+using SyndicDocumentsData.Models;
+// REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service" dans le code, le fichier svc et le fichier de configuration.
+public class PartsFile
+{
+    public String Key = "";
+    public byte[] Content;
+    public PartsFile(string Key)
+    {
+        this.Key = Key;
+    }
+}
+public class Service : IService
+{
+    static List<PartsFile> parts = new List<PartsFile>();
+
+    public string CreateUser(string Code, String Password = "")
+    {
+        string msg = "";
+        SyndicDocumentsData.Models.IDal dal = new SyndicDocumentsData.Models.Dal();
+        msg = dal.CreateUser(Code, Password);
+        return msg;
+    }
+    public string DeleteUser(string Guid)
+    {
+        string msg = "";
+        SyndicDocumentsData.Models.IDal dal = new SyndicDocumentsData.Models.Dal();
+        msg = dal.DeleteUser(Guid);
+        return msg;
+    }
+    public string DeleteCopro(string Guid)
+    {
+        string msg = "";
+        SyndicDocumentsData.Models.IDal dal = new SyndicDocumentsData.Models.Dal();
+        msg = dal.DeleteCopro(Guid);
+        return msg;
+    }
+    public void UploadPartFile(string Guid, byte[] filePart)
+    {
+        //Console.WriteLine("Uploaded file {0} with {1} bytes", fileName, totalBytesRead);
+        PartsFile part = null;
+        int offset = 0;
+        if (parts.Exists(x => x.Key == Guid))
+        {
+            part = parts.Find(x => x.Key == Guid);
+            offset = part.Content.Length;
+            System.Array.Resize(ref part.Content, part.Content.Length + filePart.Length);
+        }
+        else
+        {
+            part = new PartsFile(Guid);
+            part.Content = new byte[filePart.Length];
+            parts.Add(part);
+        }
+        System.Buffer.BlockCopy(filePart, 0, part.Content, offset, filePart.Length);
+    }
+    public String CloseFile(string text, String Guid, string immeuble_id, string copro_id)
+    {
+        String resText = "";
+        if (parts.Exists(x => x.Key == Guid))
+        {
+            try
+            {
+                PartsFile part = parts.Find(x => x.Key == Guid);
+                IDal dal = new Dal();
+                dal.CreateDocument(text, Guid, part.Content, immeuble_id, copro_id);
+                resText = "Document Enregistré";
+            }
+            catch (Exception ex)
+            {
+                resText = ex.Message;
+            }
+        }
+        else
+            resText = "Guid Invalide";
+        return resText;
+    }
+    public UserEntitie GetUserFromCode(String code)
+    {
+        IDal dal = new Dal();
+        return dal.GetUserFromCode(code);
+    }
+
+    public UserEntitie GetUserFromGuid(String Guid)
+    {
+        IDal dal = new Dal();
+        return dal.GetUserFromGuid(Guid);
+    }
+    public List<UserEntitie> GetAllUsers()
+    {
+        IDal dal = new Dal();
+        return dal.GetAllUsers();
+    }
+    public List<ChildrenEntitie> GetUserCoproprietaires(string guid)
+    {
+        List<ChildrenEntitie> childrens = new List<ChildrenEntitie>();
+        IDal dal = new Dal();
+        UserEntitie user = dal.GetUserFromGuid(guid);
+        if (user != null)
+        {
+            childrens = dal.GetUserChildrens(user.Guid);
+        }
+        return childrens;
+    }
+    public string AddCopro(string user_id, string immeuble_id, String copro_id, string reference_imm, string adresse_imm, string reference_copro, string nom)
+    {
+        IDal dal = new Dal();
+        return dal.AddCopro(user_id, immeuble_id, copro_id, reference_imm, adresse_imm, reference_copro, nom);
+    }
+    public bool Connected()
+    {
+        return true;
+    }
+    public string UpdateUser(string Guid, string Code, String Password = "")
+    {
+        string msg = "";
+        SyndicDocumentsData.Models.IDal dal = new SyndicDocumentsData.Models.Dal();
+        msg = dal.UpdateUser(Guid, Code, Password);
+        return msg;
+    }
+    public List<DocumentEntitie> GetListDocuments(string immeuble_id = "", string copro_id ="")
+    {
+        IDal dal = new Dal();
+        return dal.GetDocuments(immeuble_id, copro_id);
+    }
+    public string DeleteDocument(string Guid)
+    {
+        string msg = "";
+        IDal dal = new Dal();
+        msg = dal.DeleteDocument(Guid);
+        return msg;
+    }
+    
+}
