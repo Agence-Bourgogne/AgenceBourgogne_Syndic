8e67431320c3f2fd7305069b775b4d5d50a8702e
Enzo Sandre <enzo@enzosandre.fr>
2026-08-12 15:10:00 +0200

    Factrisation

---
+++ GrandLivre.Templates/Helpers/DecimalExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace GrandLivre.Templates.Helpers;
+
+public static class DecimalExtensions
+{
+    public static string ToInsecableCurrency(this decimal d) 
+        => d.ToString("C").Replace(" ", "\u00A0");
+}
